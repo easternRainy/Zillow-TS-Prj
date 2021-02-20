7c1ad7f915b3322853af662e869f94da8e83d00b
zhousicheng <github01.vacuolar@simplelogin.fr>
2021-02-20 12:04:52 -0800

    update configs for SARIMA model based on EDA

--- configs.py
+++ configs.py
@@ -3,22 +3,23 @@ from metrics import *
 ## configurations
 
 # All metrics in SARIMA model
-p_values = [3,4]
-d_values = [1]
-q_values = [0,1,2]
+p_values = [0,1,2,3]
+d_values = [2]
+q_values = [0,1,2,3,4,5]
 P_values = [0,1]
-D_values = [0]
+D_values = [7]
 Q_values = [0,1]
-m_values = [7]
+m_values = [10]
 
 
 # output_name is the name of csv table containing each combination of parameters and the error metrics
 # we could select model by sorting this table on error metrics
-output_name = "model_selection.csv"
+output_name = "model_selection_rmse.csv"
 
 # all metrics are defined in metrics.py, like RMSE, MAE, etc.
 metrics_method = RMSE
 
 # get the data we need
-pharm_dt=pd.read_csv('pharm_history.csv',index_col='datum', parse_dates=True,squeeze=True)
-data = pharm_dt['N05B'].values
+main_col = 'MedianSoldPrice_AllHomes.California'
+df = pd.read_csv("data/df_train_one_col_split.csv", index_col=["Date"], parse_dates=["Date"])
+data = df[main_col].values
